e7874782523a34ed81368e053be98b9db4f5e673
Demi <kizura.vian@gmail.com>
2018-09-15 22:51:58 +0800

    Update install-premiumscript.sh

--- install-premiumscript.sh
+++ install-premiumscript.sh
@@ -22,8 +22,8 @@ END3
 
 cd /usr/local/bin
 wget -O premium-script.tar.gz "https://raw.githubusercontent.com/musivian/premscript/master/premium-script_1.tar.gz"
-tar -xvf premium-script.tar.gz
-rm -f premium-script.tar.gz
+tar -xvf premium-script_1.tar.gz
+rm -f premium-script_1.tar.gz
 
 cp /usr/local/bin/premium-script /usr/local/bin/menu
 
